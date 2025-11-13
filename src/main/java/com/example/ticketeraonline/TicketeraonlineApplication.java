5a44d7f84496a49985fa0f3cce7c518c2a4d8c06
santiagobot321 <boteroramirezsantiago@gmail.com>
2025-11-13 15:31:54 -0500

    creating the project structure

---
+++ src/main/java/com/example/ticketeraonline/TicketeraonlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketeraonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketeraonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketeraonlineApplication.class, args);
+	}
+
+}

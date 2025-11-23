df71ead805d0c376d802c5e99aff4a3c4e50ba8c
santiagobot321 <boteroramirezsantiago@gmail.com>
2025-11-22 19:17:05 -0500

    finishing HU3

---
+++ src/main/java/com/example/ticketeraonline/dominio/Venue.java
@@ -0,0 +1,11 @@
+package com.example.ticketeraonline.dominio;
+
+import lombok.Data;
+
+@Data
+public class Venue {
+    private Long id;
+    private String name;
+    private String address;
+    private int capacity;
+}
